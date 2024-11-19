0194e94a3d2898795e5a03082cf6f2c1b741de4b
Sprocket06 <lpolar45@gmail.com>
2024-11-19 16:01:12 -0500

    icons + which-key

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -7,5 +7,26 @@ return {
 	{
     		"nvim-telescope/telescope-file-browser.nvim",
     		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+	{
+		"nvim-tree/nvim-web-devicons"
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+		  -- your configuration comes here
+		  -- or leave it empty to use the default settings
+		    -- refer to the configuration section below
+		},
+		keys = {
+		  {
+		    "<leader>?",
+		     function()
+			require("which-key").show({ global = false })
+		     end,
+		      desc = "Buffer Local Keymaps (which-key)",
+		  },
+		},
 	}
 }
